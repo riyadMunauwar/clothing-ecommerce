8c3edd6bb1a04d3ec1db9ada1fa2b1920f9b808c
riyad <hifazakhatun1108@gmail.com>
2023-09-08 11:33:27 +0600

    deploy

--- resources/views/front/pages/home.blade.php
+++ resources/views/front/pages/home.blade.php
@@ -4,6 +4,7 @@
     @include('front.partials.intro-slider')
     @include('front.partials.brands-slider')
     <livewire:front.trendy-products-with-caurosel />
+    @include('front.partials.shop-by-categories')
     <livewire:front.recent-arrival-products />
     @include('front.partials.selling-feature-banner')
 
