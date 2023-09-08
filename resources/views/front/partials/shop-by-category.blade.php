4125466b772865d139c725f155e811aef23cd693
riyad <hifazakhatun1108@gmail.com>
2023-09-08 11:30:25 +0600

    deploy

---
+++ resources/views/front/partials/shop-by-category.blade.php
@@ -0,0 +1,57 @@
+<section>
+    <div class="container categories pt-6">
+        <h2 class="title-lg text-center mb-4">Shop by Categories</h2><!-- End .title-lg text-center -->
+
+        <div class="row">
+            <div class="col-6 col-lg-4">
+                <div class="banner banner-display banner-link-anim">
+                    <a href="#">
+                        <img src="{{ asset('assets/images/banners/home/banner-1.jpg')" alt="Banner">
+                    </a>
+
+                    <div class="banner-content banner-content-center">
+                        <h3 class="banner-title text-white"><a href="#">Outdoor</a></h3><!-- End .banner-title -->
+                        <a href="#" class="btn btn-outline-white banner-link">Shop Now<i class="icon-long-arrow-right"></i></a>
+                    </div><!-- End .banner-content -->
+                </div><!-- End .banner -->
+            </div><!-- End .col-sm-6 col-lg-3 -->
+            <div class="col-6 col-lg-4 order-lg-last">
+                <div class="banner banner-display banner-link-anim">
+                    <a href="#">
+                        <img src="{{ asset('assets/images/banners/home/banner-4.jpg') }}" alt="Banner">
+                    </a>
+
+                    <div class="banner-content banner-content-center">
+                        <h3 class="banner-title text-white"><a href="#">Lighting</a></h3><!-- End .banner-title -->
+                        <a href="#" class="btn btn-outline-white banner-link">Shop Now<i class="icon-long-arrow-right"></i></a>
+                    </div><!-- End .banner-content -->
+                </div><!-- End .banner -->
+            </div><!-- End .col-sm-6 col-lg-3 -->
+            <div class="col-sm-12 col-lg-4 banners-sm">
+                <div class="row">
+                    <div class="banner banner-display banner-link-anim col-lg-12 col-6">
+                        <a href="#">
+                            <img src="{{ asset('assets/images/banners/home/banner-2.jpg') }}" alt="Banner">
+                        </a>
+
+                        <div class="banner-content banner-content-center">
+                            <h3 class="banner-title text-white"><a href="#">Furniture and Design</a></h3><!-- End .banner-title -->
+                            <a href="#" class="btn btn-outline-white banner-link">Shop Now<i class="icon-long-arrow-right"></i></a>
+                        </div><!-- End .banner-content -->
+                    </div><!-- End .banner -->
+
+                    <div class="banner banner-display banner-link-anim col-lg-12 col-6">
+                        <a href="#">
+                            <img src="{{ asset('assets/images/banners/home/banner-3.jpg') }}" alt="Banner">
+                        </a>
+
+                        <div class="banner-content banner-content-center">
+                            <h3 class="banner-title text-white"><a href="#">Kitchen & Utensil</a></h3><!-- End .banner-title -->
+                            <a href="#" class="btn btn-outline-white banner-link">Shop Now<i class="icon-long-arrow-right"></i></a>
+                        </div><!-- End .banner-content -->
+                    </div><!-- End .banner -->
+                </div>
+            </div><!-- End .col-sm-6 col-lg-3 -->
+        </div><!-- End .row -->
+    </div><!-- End .container -->
+</section>
