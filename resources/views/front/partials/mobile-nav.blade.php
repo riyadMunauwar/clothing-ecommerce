48972c4f00813400f6e4aea65467eafdf1f648c2
riyad <hifazakhatun1108@gmail.com>
2023-09-12 10:47:29 +0600

    deploy

--- resources/views/front/partials/mobile-nav.blade.php
+++ resources/views/front/partials/mobile-nav.blade.php
@@ -30,7 +30,7 @@
                     @if(!count($children) > 0)
                         @if($menu->category)
                             <li>
-                                <a href="#">{{ $menu->name }}</a>
+                                <a href="{{ route('category', ['category_slug' => $menu->category->slug, 'id' => $menu->category->id]) }}">{{ $menu->name }}</a>
                             </li>
                         @else 
                             <li>
@@ -53,7 +53,7 @@
                                     @if(!count($grandChildren) > 0)
 
                                         @if($child->category)
-                                            <li><a href="#">{{ $child->name }}</a></li>
+                                            <li><a href="{{ route('category', ['category_slug' => $child->category->slug, 'id' => $child->category->id]) }}">{{ $child->name }}</a></li>
                                         @else
                                             <li><a href="{{ $child->link }}">{{ $child->name }}</a></li>
                                         @endif
@@ -66,7 +66,7 @@
                                                 @foreach($grandChildren as $grandChild)
 
                                                     @if($grandChild->category)
-                                                        <li><a href="#">{{ $grandChild->name }}</a></li>
+                                                        <li><a href="{{ route('category', ['category_slug' => $grandChild->category->slug, 'id' => $grandChild->category->id]) }}">{{ $grandChild->name }}</a></li>
                                                     @else
                                                         <li><a href="{{ $grandChild->link }}">{{ $grandChild->name }}</a></li>
                                                     @endif
