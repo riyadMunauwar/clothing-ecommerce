482828b15c3f5931e0f9ea17a2a933228ea1f2cd
riyad <hifazakhatun1108@gmail.com>
2023-09-13 12:34:04 +0600

    deploy

--- resources/views/front/components/product-details.blade.php
+++ resources/views/front/components/product-details.blade.php
@@ -72,6 +72,22 @@
                                 <a href="#" class="size-guide"><i class="icon-th-list"></i>size guide</a>
                             </div><!-- End .details-filter-row -->
 
+
+                            <div class="details-filter-row details-row-size">
+                                <label for="size">Size:</label>
+                                <div class="select-custom">
+                                    <select name="size" id="size" class="form-control">
+                                        <option value="#" selected="selected">Select a size</option>
+                                        <option value="s">Small</option>
+                                        <option value="m">Medium</option>
+                                        <option value="l">Large</option>
+                                        <option value="xl">Extra Large</option>
+                                    </select>
+                                </div><!-- End .select-custom -->
+
+                                <a href="#" class="size-guide"><i class="icon-th-list"></i>size guide</a>
+                            </div><!-- End .details-filter-row -->
+
                             <div class="product-details-action">
                                 <div class="details-action-col">
                                     <label for="qty">Qty:</label>
