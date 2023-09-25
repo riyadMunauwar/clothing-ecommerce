424fc155759b63ef4016f7221ce3064630e27bfc
riyad <hifazakhatun1108@gmail.com>
2023-09-25 10:20:34 +0600

    deploy

--- app/Http/Livewire/Front/FeaturedProducts.php
+++ app/Http/Livewire/Front/FeaturedProducts.php
@@ -13,7 +13,7 @@ class FeaturedProducts extends Component
     public function render()
     {
         $products = $this->getProducts();
-        dd($products->nextPageUrl(), $products->url());
+        dd($products->nextPageUrl(), $products->url(3));
         return view('front.components.featured-products', compact('products'));
     }
 
