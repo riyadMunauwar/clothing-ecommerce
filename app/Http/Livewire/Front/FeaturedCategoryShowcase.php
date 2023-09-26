e4b6014441ae1b0dc3efe1ff5e08448e6a9ea3ae
riyad <hifazakhatun1108@gmail.com>
2023-09-26 12:30:32 +0600

    deploy

--- app/Http/Livewire/Front/FeaturedCategoryShowcase.php
+++ app/Http/Livewire/Front/FeaturedCategoryShowcase.php
@@ -8,18 +8,11 @@ use App\Models\Category;
 class FeaturedCategoryShowcase extends Component
 {
 
-    public $featured_category_one;
-    public $featured_category_two;
-    public $featured_category_three;
-    public $featured_category_four;
-    public $featured_category_five;
-    public $featured_category_six;
-    public $featured_category_seven;
-    public $featured_category_eight;
+    public $categories = [];
 
     public function mount()
     {
-        $this->getFeaturedCategories();
+        $this->categories = $this->getFeaturedCategories();
     }
 
     public function render()
@@ -29,36 +22,7 @@ class FeaturedCategoryShowcase extends Component
 
     private function getFeaturedCategories()
     {
-        $this->featured_category_one = Category::withCount('products')->published()->featured()->first();
-
-        if($this->featured_category_one){
-            $this->featured_category_two = Category::withCount('products')->published()->featured()->whereNotIn('id', [$this->featured_category_one->id])->first();
-        }
-
-        if($this->featured_category_two){
-            $this->featured_category_three = Category::withCount('products')->published()->featured()->whereNotIn('id', [$this->featured_category_two->id])->first();
-        }
-
-        if($this->featured_category_three){
-            $this->featured_category_four = Category::withCount('products')->published()->featured()->whereNotIn('id', [$this->featured_category_three->id])->first();
-        }
-        
-        if($this->featured_category_four){
-            $this->featured_category_five = Category::withCount('products')->published()->featured()->whereNotIn('id', [$this->featured_category_four->id])->first();
-        }
-
-        if($this->featured_category_five){
-            $this->featured_category_six = Category::withCount('products')->published()->featured()->whereNotIn('id', [$this->featured_category_five->id])->first();
-        }
-
-        if($this->featured_category_six){
-            $this->featured_category_seven = Category::withCount('products')->published()->featured()->whereNotIn('id', [$this->featured_category_six->id])->first();
-        }
-
-        if($this->featured_category_seven){
-            $this->featured_category_eight = Category::withCount('products')->published()->featured()->whereNotIn('id', [$this->featured_category_seven->id])->first();
-        }
-        
+        return Category::withCount('products')->published()->featured()->take(8);
     }
 
 
--- resources/views/front/components/featured-category-showcase.blade.php
+++ resources/views/front/components/featured-category-showcase.blade.php
@@ -2,130 +2,25 @@
     <div class="categories-page">
         <div class="container-fluid">
             <div class="row">
-                <div class="col-lg-6">
-                    <div class="row">
-                        @if($featured_category_one)
-                        <div class="col-sm-8">
+
+                @foreach($categories as $category)
+
+                        <div class="col-sm-4 col-md-2">
                             <div class="banner banner-cat banner-badge">
                                 <a href="#">
-                                    <img src="{{ $featured_category_one->iconUrl('medium') }}" alt="{{ $featured_category_one->name }}">
+                                    <img src="{{ $category->iconUrl('medium') }}" alt="{{ $category->name }}">
                                 </a>
 
                                 <a class="banner-link" href="#">
-                                    <h3 class="banner-title">{{ $featured_category_one->name }}</h3><!-- End .banner-title -->
-                                    <h4 class="banner-subtitle">{{ $featured_category_one->count_products }} Products</h4><!-- End .banner-subtitle -->
-                                    <span class="banner-link-text">Shop Now</span>
-                                </a><!-- End .banner-link -->
-                            </div><!-- End .banner -->
-                        </div><!-- End .col-sm-8 -->
-                        @endif
-
-                        @if($featured_category_two)
-                            <div class="col-sm-4">
-                                <div class="banner banner-cat banner-badge">
-                                    <a href="#">
-                                        <img src="{{ $featured_category_two->iconUrl('medium') }}" alt="{{ $featured_category_two->name }}">
-                                    </a>
-
-                                    <a class="banner-link" href="#">
-                                        <h3 class="banner-title">{{ $featured_category_two->name }}</h3><!-- End .banner-title -->
-                                        <h4 class="banner-subtitle">{{ $featured_category_two->count_products }} Products</h4><!-- End .banner-subtitle -->
-                                        <span class="banner-link-text">Shop Now</span>
-                                    </a><!-- End .banner-link -->
-                                </div><!-- End .banner -->
-                            </div><!-- End .col-sm-4 -->
-                        @endif
-
-                        @if($featured_category_three)
-                            <div class="col-sm-4">
-                                <div class="banner banner-cat banner-badge">
-                                    <a href="#">
-                                        <img src="{{ $featured_category_three->iconUrl('medium') }}" alt="{{ $featured_category_three->name }}">
-                                    </a>
-
-                                    <a class="banner-link" href="#">
-                                        <h3 class="banner-title">{{ $featured_category_three->name }}</h3><!-- End .banner-title -->
-                                        <h4 class="banner-subtitle">{{ $featured_category_three->count_products }} Products</h4><!-- End .banner-subtitle -->
-                                        <span class="banner-link-text">Shop Now</span>
-                                    </a><!-- End .banner-link -->
-                                </div><!-- End .banner -->
-                            </div><!-- End .col-sm-4 -->
-                        @endif
-
-                        @if($featured_category_four)
-                            <div class="col-sm-8">
-                                <div class="banner banner-cat banner-badge">
-                                    <a href="#">
-                                        <img src="{{ $featured_category_four->iconUrl('medium') }}" alt="{{ $featured_category_four->name }}">
-                                    </a>
-
-                                    <a class="banner-link" href="#">
-                                        <h3 class="banner-title">{{ $featured_category_four->name }}</h3><!-- End .banner-title -->
-                                        <h4 class="banner-subtitle">{{ $featured_category_four->count_products }} Products</h4><!-- End .banner-subtitle -->
-                                        <span class="banner-link-text">Shop Now</span>
-                                    </a><!-- End .banner-link -->
-                                </div><!-- End .banner -->
-                            </div><!-- End .col-sm-4 -->
-                        @endif
-
-                    </div><!-- End .row -->
-                </div><!-- End .col-lg-6 -->
-
-                <div class="col-lg-6">
-                    <div class="row">
-                        <div class="col-sm-8">
-                            <div class="banner banner-cat banner-badge">
-                                <a href="#">
-                                    <img src="{{ asset('assets/images/category/fullwidth-page/banner-5.jpg') }}" alt="Banner">
-                                </a>
-
-                                <a class="banner-link" href="#">
-                                    <h3 class="banner-title">Dresses</h3><!-- End .banner-title -->
-                                    <h4 class="banner-subtitle">3 Products</h4><!-- End .banner-subtitle -->
-                                    <span class="banner-link-text">Shop Now</span>
-                                </a><!-- End .banner-link -->
-                            </div><!-- End .banner -->
-
-                            <div class="banner banner-cat banner-badge">
-                                <a href="#">
-                                    <img src="{{ asset('assets/images/category/fullwidth-page/banner-6.jpg') }}" alt="Banner">
-                                </a>
-
-                                <a class="banner-link" href="#">
-                                    <h3 class="banner-title">Shoes</h3><!-- End .banner-title -->
-                                    <h4 class="banner-subtitle">2 Products</h4><!-- End .banner-subtitle -->
+                                    <h3 class="banner-title">{{ $category->name }}</h3><!-- End .banner-title -->
+                                    <h4 class="banner-subtitle">{{ $category->count_products }} Products</h4><!-- End .banner-subtitle -->
                                     <span class="banner-link-text">Shop Now</span>
                                 </a><!-- End .banner-link -->
                             </div><!-- End .banner -->
                         </div><!-- End .col-sm-8 -->
 
-                        <div class="col-sm-4">
-                            <div class="banner banner-cat banner-badge">
-                                <a href="#">
-                                    <img src="{{ asset('assets/images/category/fullwidth-page/banner-7.jpg') }}" alt="Banner">
-                                </a>
+                @endofreach
 
-                                <a class="banner-link" href="#">
-                                    <h3 class="banner-title">T-shirts</h3><!-- End .banner-title -->
-                                    <h4 class="banner-subtitle">0 Products</h4><!-- End .banner-subtitle -->
-                                    <span class="banner-link-text">Shop Now</span>
-                                </a><!-- End .banner-link -->
-                            </div><!-- End .banner -->
-
-                            <div class="banner banner-cat banner-badge">
-                                <a href="#">
-                                    <img src="{{ asset('assets/images/category/fullwidth-page/banner-8.jpg') }}" alt="Banner">
-                                </a>
-
-                                <a class="banner-link" href="#">
-                                    <h3 class="banner-title">Jumpers</h3><!-- End .banner-title -->
-                                    <h4 class="banner-subtitle">1 Product</h4><!-- End .banner-subtitle -->
-                                    <span class="banner-link-text">Shop Now</span>
-                                </a><!-- End .banner-link -->
-                            </div><!-- End .banner -->
-                        </div><!-- End .col-sm-4 -->
-                    </div><!-- End .row -->
-                </div><!-- End .col-lg-6 -->
             </div><!-- End .row -->
         </div><!-- End .container-fluid -->
     </div><!-- End .categories-page -->
