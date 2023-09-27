c18dd1006ad99031d0190f1e71ddcfae3ca94c33
riyad <hifazakhatun1108@gmail.com>
2023-09-27 12:33:22 +0600

    dep

--- resources/views/front/components/add-to-cart-button.blade.php
+++ resources/views/front/components/add-to-cart-button.blade.php
@@ -1,3 +1,3 @@
 <div class="product-action">
-    <button wire:click.debounce="addToCartHandeler" class="btn-product btn-cart"><span wire:loading.remove wire:target="addToCartHandeler">add to cart</span><span wire:loading wire:target="addToCartHandeler">Processing...</span></button>
+    <button wire:click.debounce="addToCartHandeler" class="btn-product btn-cart"><span wire:loading.remove wire:target="addToCartHandeler" >add to cart</span><span wire:loading wire:target="addToCartHandeler">Processing...</span></button>
 </div>
