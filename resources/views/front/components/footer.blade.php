4158467b45bc36c33af57177d23dd13dc30b1e7d
riyad <hifazakhatun1108@gmail.com>
2023-09-27 10:25:44 +0600

    deploy

--- resources/views/front/components/footer.blade.php
+++ resources/views/front/components/footer.blade.php
@@ -12,7 +12,7 @@
                          <p>Mobile: +8801920776611(WhatsApp & Imo)</p>
                          <p>Email: customercare@rayat.com.bd </p>
 
-                        <div class="social-icons">
+                        <div class="social-icons mt-4">
                             <a href="#" class="social-icon" target="_blank" title="Facebook"><i class="icon-facebook-f"></i></a>
                             <a href="#" class="social-icon" target="_blank" title="Twitter"><i class="icon-twitter"></i></a>
                             <a href="#" class="social-icon" target="_blank" title="Instagram"><i class="icon-instagram"></i></a>
