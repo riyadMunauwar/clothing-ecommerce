24fbb41bf7d964958297a91c1f80efabdac8407b
riyad <hifazakhatun1108@gmail.com>
2023-09-28 12:51:36 +0600

    \dep

---
+++ config/shipping_methods.php
@@ -0,0 +1,25 @@
+<?php 
+
+return [
+
+    [
+        'shipper' => '',
+        'area' => 'Inside Dhaka City Corp.',
+        'estimate_delivery_time' => 'Delivery within 1-5 days',
+        'delivery_cost' => 60,
+    ],
+
+    [
+        'shipper' => 'Pathao Courier (Home Delivery)',
+        'area' => 'Outside Dhaka- সারা বাংলাদেশ',
+        'estimate_delivery_time' => 'Delivery within 1-5 days',
+        'delivery_cost' => 100,
+    ],
+
+    [
+        'shipper' => 'Pathao Courier (Home Delivery)',
+        'area' => 'Outside Dhaka- সারা বাংলাদেশ',
+        'estimate_delivery_time' => 'Delivery within 1-5 days',
+        'delivery_cost' => 100,
+    ],
+];
--- resources/views/front/components/cart.blade.php
+++ resources/views/front/components/cart.blade.php
@@ -65,59 +65,37 @@
                         <tbody>
                             <tr class="summary-subtotal">
                                 <td>Subtotal:</td>
-                                <td>$160.00</td>
+                                <td>BDT {{ number_format($sub_total, 2) }}</td>
                             </tr><!-- End .summary-subtotal -->
                             <tr class="summary-shipping">
                                 <td>Shipping:</td>
                                 <td>&nbsp;</td>
                             </tr>
 
-                            <tr class="summary-shipping-row">
-                                <td>
-                                    <div class="custom-control custom-radio">
-                                        <input type="radio" id="free-shipping" name="shipping" class="custom-control-input">
-                                        <label class="custom-control-label" for="free-shipping">Free Shipping</label>
-                                    </div><!-- End .custom-control -->
-                                </td>
-                                <td>$0.00</td>
-                            </tr><!-- End .summary-shipping-row -->
-
-                            <tr class="summary-shipping-row">
-                                <td>
-                                    <div class="custom-control custom-radio">
-                                        <input type="radio" id="standart-shipping" name="shipping" class="custom-control-input">
-                                        <label class="custom-control-label" for="standart-shipping">Standart:</label>
-                                    </div><!-- End .custom-control -->
-                                </td>
-                                <td>$10.00</td>
-                            </tr><!-- End .summary-shipping-row -->
-
-                            <tr class="summary-shipping-row">
-                                <td>
-                                    <div class="custom-control custom-radio">
-                                        <input type="radio" id="express-shipping" name="shipping" class="custom-control-input">
-                                        <label class="custom-control-label" for="express-shipping">Express:</label>
-                                    </div><!-- End .custom-control -->
-                                </td>
-                                <td>$20.00</td>
-                            </tr><!-- End .summary-shipping-row -->
-
-                            <tr class="summary-shipping-estimate">
-                                <td>Estimate for Your Country<br> <a href="dashboard.html">Change address</a></td>
-                                <td>&nbsp;</td>
-                            </tr><!-- End .summary-shipping-estimate -->
+                            @foreach(config('shipping_methods') as $method)
+                                <tr class="summary-shipping-row">
+                                    <td>
+                                        <div class="custom-control custom-radio">
+                                            <input type="radio" id="free-shipping" name="shipping" class="custom-control-input">
+                                            <label class="custom-control-label" for="free-shipping">{{ $method->shipper ?? $method->area }}</label>
+                                        </div><!-- End .custom-control -->
+                                    </td>
+                                    <td>BDT {{ $method->delivery_cost }}</td>
+                                </tr><!-- End .summary-shipping-row -->
+                            @endforeach
+    
 
                             <tr class="summary-total">
                                 <td>Total:</td>
-                                <td>$160.00</td>
+                                <td>BDT {{ number_format($total, 2) }}</td>
                             </tr><!-- End .summary-total -->
                         </tbody>
                     </table><!-- End .table table-summary -->
 
-                    <a href="checkout.html" class="btn btn-outline-primary-2 btn-order btn-block">PROCEED TO CHECKOUT</a>
+                    <a href="{{ route('checkout') }}" class="btn btn-outline-primary-2 btn-order btn-block">PROCEED TO CHECKOUT</a>
                 </div><!-- End .summary -->
 
-                <a href="category.html" class="btn btn-outline-dark-2 btn-block mb-3"><span>CONTINUE SHOPPING</span><i class="icon-refresh"></i></a>
+                <a href="/" class="btn btn-outline-dark-2 btn-block mb-3"><span>CONTINUE SHOPPING</span><i class="icon-refresh"></i></a>
             </aside><!-- End .col-lg-3 -->
         </div><!-- End .row -->
     </div><!-- End .container -->
