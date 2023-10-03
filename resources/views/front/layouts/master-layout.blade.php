f74354f3fc02b5351e7de8ca0574449e7a209c2c
riyad <hifazakhatun1108@gmail.com>
2023-10-03 09:33:10 +0600

    dep

--- resources/views/front/layouts/master-layout.blade.php
+++ resources/views/front/layouts/master-layout.blade.php
@@ -26,7 +26,7 @@
         <meta name="theme-color" content="#ffffff">
        
         <!-- End Favicon -->
-        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/favicon/apple-touch-icon.png')">
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/favicon/apple-touch-icon.png') }}">
         <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/favicon/favicon-32x32.png') }}">
         <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/favicon/favicon-16x16.png') }}">
         <link rel="manifest" href="{{ asset('assets/favicon/site.webmanifest') }}">
