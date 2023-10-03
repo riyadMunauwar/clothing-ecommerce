5f8e3064b24905b7472e55bf53ddf3c53eed7cc3
riyad <hifazakhatun1108@gmail.com>
2023-10-03 09:45:45 +0600

    dep

--- resources/views/front/layouts/master-layout.blade.php
+++ resources/views/front/layouts/master-layout.blade.php
@@ -12,25 +12,16 @@
         <!-- CSRF TOKEN -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Favicon -->
-        <link rel="apple-touch-icon" sizes="180x180" href="assets/images/icons/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="assets/images/icons/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="assets/images/icons/favicon-16x16.png">
-        <link rel="manifest" href="assets/images/icons/site.html">
-        <link rel="mask-icon" href="assets/images/icons/safari-pinned-tab.svg" color="#666666">
-        <link rel="shortcut icon" href="assets/images/icons/favicon.ico">
-        <meta name="apple-mobile-web-app-title" content="Rayat Boutique">
-        <meta name="application-name" content="Molla">
-        <meta name="msapplication-TileColor" content="#cc9966">
-        <meta name="msapplication-config" content="assets/images/icons/browserconfig.xml">
-        <meta name="theme-color" content="#ffffff">
+
+
        
         <!-- End Favicon -->
         <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/favicon/apple-touch-icon.png') }}">
         <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/favicon/favicon-32x32.png') }}">
         <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/favicon/favicon-16x16.png') }}">
         <link rel="manifest" href="{{ asset('assets/favicon/site.webmanifest') }}">
-
+        <!-- Favicon -->
+        
         {!! SEOMeta::generate() !!}
         {!! OpenGraph::generate() !!}
         {!! Twitter::generate() !!}
