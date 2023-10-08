c0782741467839061657e7826479a9fb3a04816e
riyad <hifazakhatun1108@gmail.com>
2023-10-08 10:16:45 +0600

    dep

--- resources/views/front/components/cart.blade.php
+++ resources/views/front/components/cart.blade.php
@@ -79,7 +79,7 @@
                                 <tr class="summary-shipping-row">
                                     <td>
                                         <div class="custom-control custom-radio">
-                                            <input wire:model="shipping_method" value="{{$method['shipper'] . '-' . $method['delivery_cost']}}" type="radio" id="shipping-method-{{ $index }}" name="shipping-method" class="custom-control-input">
+                                            <input wire:model.debounce="shipping_method" value="{{$method['shipper'] . '-' . $method['delivery_cost']}}" type="radio" id="shipping-method-{{ $index }}" name="shipping-method" class="custom-control-input">
                                             <label class="custom-control-label" for="shipping-method-{{ $index }}">
                                                 <span class="text-md">{{ $method['shipper'] ?? $method['area'] }}</span>
                                                 <span class="d-block text-sm"> {{ $method['area'] }} </span>
