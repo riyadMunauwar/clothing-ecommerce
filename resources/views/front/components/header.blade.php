3be45b3a979a8f41fef901a1a7d10ad2a4be725d
riyad <hifazakhatun1108@gmail.com>
2023-10-09 12:20:50 +0600

    dep

--- resources/views/front/components/header.blade.php
+++ resources/views/front/components/header.blade.php
@@ -86,9 +86,9 @@
                                             @if(!count($child->children) > 0)
 
                                                 @if($child->category)
-                                                    <li><a href="{{ route('category', ['category_slug' => $child->category->slug, 'id' => $child->category->id]) }}">{{ $child->name }}</a></li>
+                                                    <li><a class="fw-bold" href="{{ route('category', ['category_slug' => $child->category->slug, 'id' => $child->category->id]) }}">{{ $child->name }}</a></li>
                                                 @else
-                                                    <li><a href="{{ $child->link }}">{{ $child->name }}</a></li>
+                                                    <li><a class="fw-bold" href="{{ $child->link }}">{{ $child->name }}</a></li>
                                                 @endif
 
                                             @else
@@ -99,9 +99,9 @@
                                                         @foreach($child->children as $grandChild)
 
                                                             @if($grandChild->category)
-                                                                <li><a href="{{ route('category', ['category_slug' => $grandChild->category->slug, 'id' => $grandChild->category->id]) }}">{{ $grandChild->name }}</a></li>
+                                                                <li><a class="fw-bold" href="{{ route('category', ['category_slug' => $grandChild->category->slug, 'id' => $grandChild->category->id]) }}">{{ $grandChild->name }}</a></li>
                                                             @else
-                                                                <li><a href="{{ $grandChild->link }}">{{ $grandChild->name }}</a></li>
+                                                                <li><a class="fw-bold" href="{{ $grandChild->link }}">{{ $grandChild->name }}</a></li>
                                                             @endif
 
                                                         @endforeach
