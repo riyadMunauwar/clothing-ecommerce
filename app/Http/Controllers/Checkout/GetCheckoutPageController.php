80c043f4f542c2db3104b9c98cf2709ab14840b5
riyad <hifazakhatun1108@gmail.com>
2023-10-12 11:04:25 +0600

    dep

--- app/Http/Controllers/Checkout/GetCheckoutPageController.php
+++ app/Http/Controllers/Checkout/GetCheckoutPageController.php
@@ -10,7 +10,7 @@ class GetCheckoutPageController extends Controller
     public function __invoke()
     {
         if(!auth()->check()){
-            return redirect()->route('register')->with(['redirect' => 'checkout']);
+            return redirect()->route('register', ['redirect' => 'checkout'])->with('message', "If you're new to our site, please proceed by creating an account to complete. If you already have an account with us, please log in to continue with your purchase.");
         }
 
         $isSelect = session()->has('shipping_option') ? session()->get('shipping_option') : false;
