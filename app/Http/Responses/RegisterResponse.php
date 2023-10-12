2eaf1a2273caf3837461c41b472b377bf3829b46
riyad <hifazakhatun1108@gmail.com>
2023-10-12 11:47:23 +0600

    dep

--- app/Http/Responses/RegisterResponse.php
+++ app/Http/Responses/RegisterResponse.php
@@ -10,17 +10,18 @@ class RegisterResponse implements RegisterResponseContract
     public function toResponse($request)
     {
 
-        // $redirectRoute = $request->redirect;
+        $redirectRoute = $request->redirect;
 
+        return redirect()->route('checkout');
 
-        // if($redirectRoute === 'checkout'){
+        if($redirectRoute === 'checkout'){
 
-        //     return redirect()->route('cart');
-        // }
-        // else {
+            return redirect()->route('cart');
+        }
+        else {
 
-        //     return redirect()->route('checkout');
-        // }
+            return redirect()->route('checkout');
+        }
 
     }
 
