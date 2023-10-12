36c837413db1708d0256dec248cb000d6df8ed0f
riyad <hifazakhatun1108@gmail.com>
2023-10-12 12:36:29 +0600

    dep

--- resources/views/front/components/checkout.blade.php
+++ resources/views/front/components/checkout.blade.php
@@ -172,7 +172,7 @@
 
                         <button wire:click.debounce="startPayment" type="button" class="btn btn-outline-primary-2 btn-order btn-block">
                             <span class="btn-text" wire:loading.remove wire:target="startPayment">Place Order</span>
-                            <span class="btn-text" wire:loading wire:target="startPayment">Place Order</span>
+                            <span class="btn-text" wire:loading wire:target="startPayment">Procesing...</span>
                             <span class="btn-hover-text" wire:loading.remove wire:target="startPayment">Proceed for payment</span>
                         </button>
                     </div><!-- End .summary -->
