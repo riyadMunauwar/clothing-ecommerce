a2d8f4cc91ac4bc128fadcf54ac5898d7efde2bb
riyad <hifazakhatun1108@gmail.com>
2023-10-15 10:24:23 +0600

    dep

--- resources/views/front/components/social-links.blade.php
+++ resources/views/front/components/social-links.blade.php
@@ -2,7 +2,7 @@
 <div class="social-icons mt-4">
     @foreach($socialLinks as $socialLink)
 
-        {{ dd($this->getMainDomain($socialLink)) }}
+        {{ dd($this->getMainDomain($socialLink->link)) }}
         <a href="https://www.facebook.com/rayatboutique" class="social-icon" target="_blank" title="Facebook"><i class="icon-facebook-f"></i></a>
         <a href="https://twitter.com/rayatboutique" class="social-icon" target="_blank" title="Twitter"><i class="icon-twitter"></i></a>
         <a href="https://www.instagram.com/rayatboutiquebd/" class="social-icon" target="_blank" title="Instagram"><i class="icon-instagram"></i></a>
