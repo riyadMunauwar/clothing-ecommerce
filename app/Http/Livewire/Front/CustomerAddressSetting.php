798e7d3280dee6e1501d1fb3ab228ed1d6ac00d5
riyad <hifazakhatun1108@gmail.com>
2023-10-15 12:47:36 +0600

    dep

--- app/Http/Livewire/Front/CustomerAddressSetting.php
+++ app/Http/Livewire/Front/CustomerAddressSetting.php
@@ -3,9 +3,17 @@
 namespace App\Http\Livewire\Front;
 
 use Livewire\Component;
+use App\Models\Address;
 
 class CustomerAddressSetting extends Component
 {
+    public $addresses = [];
+
+    public function mount()
+    {
+        $this->addresses = Address::where('user_id', auth()->id())->get();
+    }
+
     public function render()
     {
         return view('front.components.customer-address-setting');
--- resources/views/front/components/customer-address-setting.blade.php
+++ resources/views/front/components/customer-address-setting.blade.php
@@ -1,28 +1,37 @@
 <div class="row">
+
+    @foreach($addresses as $address)
+        <div class="col-lg-6">
+            <div class="card card-dashboard">
+                <div class="card-body">
+                    <h3 class="card-title">Address {{ ++$loopt->index }}</h3><!-- End .card-title -->
+
+                    <p>{{ $address->name }}<br>
+                    {{ $address->mobile_no }}<br>
+                    {{ $address->email }}<br>
+                    {{ $address->street }}<br>
+                    {{ $address->zip }}<br>
+                    {{ $address->city }}<br>
+                    {{ $address->state }}<br>
+                    {{ $address->country }}<br>
+                    <!-- <a href="https://portotheme.com/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="31485e44435c50585d715c50585d1f525e5c">[email&#160;protected]</a><br> -->
+                    <!-- <a href="#">Edit <i class="icon-edit"></i></a></p> -->
+                </div>
+            </div><!-- End .card-dashboard -->
+        </div><!-- End .col-lg-6 -->
+    @endforeach
+
+
+<!-- 
     <div class="col-lg-6">
         <div class="card card-dashboard">
             <div class="card-body">
-                <h3 class="card-title">Billing Address</h3><!-- End .card-title -->
-
-                <p>User Name<br>
-                User Company<br>
-                John str<br>
-                New York, NY 10001<br>
-                1-234-987-6543<br>
-                <a href="https://portotheme.com/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="31485e44435c50585d715c50585d1f525e5c">[email&#160;protected]</a><br>
-                <a href="#">Edit <i class="icon-edit"></i></a></p>
-            </div><!-- End .card-body -->
-        </div><!-- End .card-dashboard -->
-    </div><!-- End .col-lg-6 -->
-
-    <div class="col-lg-6">
-        <div class="card card-dashboard">
-            <div class="card-body">
-                <h3 class="card-title">Shipping Address</h3><!-- End .card-title -->
+                <h3 class="card-title">Shipping Address</h3>
 
                 <p>You have not set up this type of address yet.<br>
                 <a href="#">Edit <i class="icon-edit"></i></a></p>
-            </div><!-- End .card-body -->
-        </div><!-- End .card-dashboard -->
-    </div><!-- End .col-lg-6 -->
+            </div>
+        </div>
+    </div> -->
+
 </div><!-- End .row -->
