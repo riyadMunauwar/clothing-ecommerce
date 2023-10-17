1897884e7de34a5f240847940c336a12c105d760
riyad <hifazakhatun1108@gmail.com>
2023-10-17 11:01:24 +0600

    dep

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
-    'role:admin,editor,manager'
+    // 'role:admin,editor,manager'
 ])->group(function () {
 
     Route::view('/dashboard', 'admin.pages.dashboard')->name('dashboard');
@@ -64,7 +64,7 @@ Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
-    'role:user,admin,editor,manager'
+    // 'role:user,admin,editor,manager'
 ])->group(function () {
 
     // User Profile
