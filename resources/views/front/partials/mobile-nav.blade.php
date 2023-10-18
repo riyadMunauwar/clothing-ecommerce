2e8533998dac93585e7e2e5d6ddaf347cd376f21
riyad <hifazakhatun1108@gmail.com>
2023-10-18 12:50:23 +0600

    dep

--- resources/views/front/partials/mobile-nav.blade.php
+++ resources/views/front/partials/mobile-nav.blade.php
@@ -4,9 +4,9 @@
     <div class="mobile-menu-wrapper">
         <span class="mobile-menu-close"><i class="icon-close"></i></span>
 
-        <form action="#" method="get" class="mobile-search">
+        <form action="route('search')" method="GET" class="mobile-search">
             <label for="mobile-search" class="sr-only">Search</label>
-            <input type="search" class="form-control" name="mobile-search" id="mobile-search" placeholder="Search in..." required>
+            <input type="search" class="form-control" name="search_query" id="mobile-search" placeholder="Search in..." required>
             <button class="btn btn-primary" type="submit"><i class="icon-search"></i></button>
         </form>
         
@@ -24,7 +24,7 @@
                     @if(!count($menu->children) > 0)
                         @if($menu->category)
                             <li>
-                                <a href="{{ route('category', ['category_slug' => $menu->category->slug, 'id' => $menu->category->id]) }}">{{ $menu->name }}</a>
+                                <a href="{{ route('category_id', ['category_slug' => $menu->category_slug, 'id' => $menu->category_id]) }}">{{ $menu->name }}</a>
                             </li>
                         @else 
                             <li>
@@ -41,8 +41,8 @@
 
                                     @if(!count($child->children) > 0)
 
-                                        @if($child->category)
-                                            <li><a href="{{ route('category', ['category_slug' => $child->category->slug, 'id' => $child->category->id]) }}">{{ $child->name }}</a></li>
+                                        @if($child->category_id)
+                                            <li><a href="{{ route('category', ['category_slug' => $child->category_slug, 'id' => $child->category_id]) }}">{{ $child->name }}</a></li>
                                         @else
                                             <li><a href="{{ $child->link }}">{{ $child->name }}</a></li>
                                         @endif
@@ -54,8 +54,8 @@
                                             <ul>
                                                 @foreach($child->children as $grandChild)
 
-                                                    @if($grandChild->category)
-                                                        <li><a href="{{ route('category', ['category_slug' => $grandChild->category->slug, 'id' => $grandChild->category->id]) }}">{{ $grandChild->name }}</a></li>
+                                                    @if($grandChild->category_id)
+                                                        <li><a href="{{ route('category', ['category_slug' => $grandChild->category_slug, 'id' => $grandChild->category_id]) }}">{{ $grandChild->name }}</a></li>
                                                     @else
                                                         <li><a href="{{ $grandChild->link }}">{{ $grandChild->name }}</a></li>
                                                     @endif
@@ -72,11 +72,13 @@
             </ul>
         </nav><!-- End .mobile-nav -->
 
-        <div class="social-icons">
+        <!-- <div class="social-icons">
             <a href="#" class="social-icon" target="_blank" title="Facebook"><i class="icon-facebook-f"></i></a>
             <a href="#" class="social-icon" target="_blank" title="Twitter"><i class="icon-twitter"></i></a>
             <a href="#" class="social-icon" target="_blank" title="Instagram"><i class="icon-instagram"></i></a>
             <a href="#" class="social-icon" target="_blank" title="Youtube"><i class="icon-youtube"></i></a>
-        </div><!-- End .social-icons -->
+        </div> -->
+
+        <livewire:front.social-links />
     </div><!-- End .mobile-menu-wrapper -->
 </div><!-- End .mobile-menu-container -->
