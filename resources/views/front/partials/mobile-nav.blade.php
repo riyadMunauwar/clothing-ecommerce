a5aac4cc68f6a000a729f57daf0bd07fb46ae88d
riyad <hifazakhatun1108@gmail.com>
2023-10-18 12:53:18 +0600

    dep

--- resources/views/front/partials/mobile-nav.blade.php
+++ resources/views/front/partials/mobile-nav.blade.php
@@ -4,7 +4,7 @@
     <div class="mobile-menu-wrapper">
         <span class="mobile-menu-close"><i class="icon-close"></i></span>
 
-        <form action="route('search')" method="GET" class="mobile-search">
+        <form action="{{ route('search') }}" method="GET" class="mobile-search">
             <label for="mobile-search" class="sr-only">Search</label>
             <input type="search" class="form-control" name="search_query" id="mobile-search" placeholder="Search in..." required>
             <button class="btn btn-primary" type="submit"><i class="icon-search"></i></button>
