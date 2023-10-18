f13b49c53d030a377c4f489f32a8eca1099a5e7b
riyad <hifazakhatun1108@gmail.com>
2023-10-18 11:13:12 +0600

    dep

--- app/Http/Livewire/Front/Header.php
+++ app/Http/Livewire/Front/Header.php
@@ -40,7 +40,7 @@ class Header extends Component
             ->with('category')
             ->where('parent_id', $parentId)
             ->where('is_published', true)
-            ->orderBy('sort_order')
+            ->orderBy('order')
             ->get();
     
         foreach ($menuItems as $menu) {
