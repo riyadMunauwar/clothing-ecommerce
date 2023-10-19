3af9f1941caffa0393adb583aefb2e089dce6084
riyad <hifazakhatun1108@gmail.com>
2023-10-19 11:16:35 +0600

    dep

--- resources/views/front/components/product-details.blade.php
+++ resources/views/front/components/product-details.blade.php
@@ -363,18 +363,14 @@
 
 @push('scripts')
 <script>
-    <script>(function(d, s, id) {
+    
+    (function(d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) return;
     js = d.createElement(s); js.id = id;
     js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0";
     fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));</script>
+    }(document, 'script', 'facebook-jssdk'));
 
-    <!-- Your share button code -->
-    <div class="fb-share-button" 
-    data-href="https://www.your-domain.com/your-page.html" 
-    data-layout="button_count">
-    </div>
 </script>
 @endpush
