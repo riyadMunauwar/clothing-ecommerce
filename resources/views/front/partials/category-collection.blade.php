3977898e57aa00b00943770aef7276b86387f8a9
riyad <hifazakhatun1108@gmail.com>
2023-10-28 09:41:17 +0600

    dep

--- resources/views/front/partials/category-collection.blade.php
+++ resources/views/front/partials/category-collection.blade.php
@@ -4,7 +4,7 @@
             <div class="row">
                 <div class="col-lg-12">
                     <div class="row">
-                        <div class="col-sm-3">
+                        <div class="col-sm-4">
                             <div class="banner banner-cat banner-badge">
                                 <a href="#">
                                     <img src="{{ asset('assets/images/category/featured/1-shari.webp') }}" alt="Shari">
@@ -18,7 +18,7 @@
                             </div><!-- End .banner -->
                         </div><!-- End .col-sm-8 -->
 
-                        <div class="col-sm-3">
+                        <div class="col-sm-4">
                             <div class="banner banner-cat banner-badge">
                                 <a href="#">
                                     <img src="{{ asset('assets/images/category/featured/2-home-decor.webp') }}" alt="Home Decor">
@@ -32,7 +32,7 @@
                             </div><!-- End .banner -->
                         </div><!-- End .col-sm-4 -->
 
-                        <div class="col-sm-3">
+                        <div class="col-sm-4">
                             <div class="banner banner-cat banner-badge">
                                 <a href="#">
                                     <img src="{{ asset('assets/images/category/featured/2-selowar-kamiz.webp') }}" alt="Selwar Kamiz">
@@ -46,7 +46,7 @@
                             </div><!-- End .banner -->
                         </div><!-- End .col-sm-4 -->
 
-                        <div class="col-sm-3">
+                        <div class="col-sm-4">
                             <div class="banner banner-cat banner-badge">
                                 <a href="#">
                                     <img src="{{ asset('assets/images/category/featured/3-panjabi.webp') }}" alt="Panjabi">
@@ -60,7 +60,7 @@
                             </div><!-- End .banner -->
                         </div><!-- End .col-sm-8 -->
 
-                        <div class="col-sm-6">
+                        <div class="col-sm-4">
                             <div class="banner banner-cat banner-badge">
                                 <a href="#">
                                     <img src="{{ asset('assets/images/category/featured/4-shirt.webp') }}" alt="Shirt">
@@ -74,7 +74,7 @@
                             </div><!-- End .banner -->
                         </div>
 
-                        <div class="col-sm-6">
+                        <div class="col-sm-4">
                             <div class="banner banner-cat banner-badge">
                                 <a href="#">
                                     <img src="{{ asset('assets/images/category/featured/5-kids.webp') }}" alt="Kids">
